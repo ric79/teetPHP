1b1cdc40fe3e7111ea1f01581a095d114bdb8be7
Riccardo <riccardo.russo79@gmail.com>
2023-06-05 19:01:07 +0200

    test

--- officials/na/2023-06-05/07-28-09_Phase2/index.php
+++ officials/na/2023-06-05/07-28-09_Phase2/index.php
@@ -8,11 +8,11 @@
 <br>
 <h2>MOLTO BELLO</h2>
 <br>
+
 <?php
 
-PATH:<br>
-echo getcwd() . "\n";
-<br>
+echo "PATH:<br>" . getcwd() . "<br>";
+
 
 $arr = array("./steps/01_check_artifact/ova-pr-na-app01.OK.html", "./steps/01_check_artifact/ova-pr-na-app01.ERR.html", "./steps/01_check_artifact/ova-pr-na-app02.OK.html", "./steps/01_check_artifact/ova-pr-na-app02.ERR.html", "./steps/02_stop_services/ova-pr-na-app01.OK.html", "./steps/02_stop_services/ova-pr-na-app01.ERR.html", "./steps/02_stop_services/ova-pr-na-app01.OK.html", "./steps/02_stop_services/ova-pr-na-app02.ERR.html");
 
@@ -26,6 +26,7 @@ foreach ($arr as &$value) {
 }
 
 ?>
+
 <br>
 FINE
 
